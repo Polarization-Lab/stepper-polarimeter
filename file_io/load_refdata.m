0486f8d4990188bb990768f8cf240a305f7a4f38
kira-hart <khart@optics.arizona.edu>
2020-08-25 13:54:40 -0700

    adding function to read in ref data

---
+++ file_io/load_refdata.m
@@ -0,0 +1,22 @@
+function [ref_mean,ref_std] = load_refdata(fn,wavelength,meas_num)
+%LOAD_IMAGEDATA this function loads image data from the hdf5 file for a
+%single wavelength. Will average over apporipriate number of frames and
+%display associated metadata
+%   fn is the filename
+%   wavelength is the wavelength in nm
+%   meas_num is the number of measurements in the file
+
+%allocate space 
+ref_mean = zeros(meas_num,1);
+ref_std = zeros(meas_num,1);
+
+for i = 1:meas_num
+    group_name   = strcat('/images/wave',num2str(wavelength),'/meas',num2str(i));
+    ref          = h5read(fn,strcat(group_name,'/refdata'));
+    
+    ref_mean(i)         = mean(ref);
+    ref_std(i)          = std(ref);
+end
+
+end
+
