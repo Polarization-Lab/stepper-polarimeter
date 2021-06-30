05878eda78e4f7d3a141f1c24b3322ea331f7260
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2021-06-30 11:08:50 -0700

    Update ReadDataImagesNoRef.m

--- calibration/ReadDataImagesNoRef.m
+++ calibration/ReadDataImagesNoRef.m
@@ -1,12 +1,12 @@
 function imgdata = ReadDataImagesNoRef(filepath, wavelength, nSteps)
 
-fn = extractAfter(filepath,"Air_Calibrations\New_Diffuser\");
+%fn = extractAfter(filepath,"Air_Calibrations\New_Diffuser\");
 
 wv = wavelength;
 
 for ii = 1:nSteps
     group_name   = strcat('/images/wave',num2str(wv),'/meas',num2str(ii));
-    images       = h5read(fn,strcat(group_name,'/imagedata'));%/mean(ref);
+    images       = h5read(filepath,strcat(group_name,'/imagedata'));%/mean(ref);
 %     test_img     = (images(701:705,701:705,1,1) + images(701:705,701:705,1,2) + images(701:705,701:705,1,3))/3;
 %     mean_img     = (images(:,:,1,1) + images(:,:,1,2) + images(:,:,1,3))/3;
    
