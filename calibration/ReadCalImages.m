5462a417af2ee137015a1d4e47020effef47152a
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2021-07-01 10:08:37 -0700

    Update ReadCalImages.m

--- calibration/ReadCalImages.m
+++ calibration/ReadCalImages.m
@@ -1,15 +1,20 @@
-function meandata = ReadCalImages(filepath,wavelength,ref,nSteps)
+function meandata = ReadCalImages(fn,wavelength,ref,nSteps)
 
 %Wavelength = scalar wavelength value
 %filepath = string to .h5 file
 %ref = 1x64 of mean reference fluxes normalized to first reference flux
 %nSteps = number of measurements taken (64 usually)
 
-fn = extractAfter(filepath,"Air_Calibrations\New_Diffuser\");
-wv = wavelength;
+fn = ['D:/Measurements/Air_Calibrations/BVO-UV_PSA-LP/BVO-UV_PSA-LPjune302021-30-Jun-2021.h5'];
+
+%fn = extractAfter(filepath,"");
+
+%wavelength = 460;
+
+%nSteps = 64
 
 for ii = 1:nSteps
-    group_name   = strcat('/images/wave',num2str(wv),'/meas',num2str(ii));
+    group_name   = strcat('/images/wave',num2str(wavelength),'/meas',num2str(ii));
     images = h5read(fn,strcat(group_name,'/imagedata'))/mean(ref);
     meandata(ii,:,:) = images;
 end
