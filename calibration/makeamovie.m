eafeb415bc4ea4f65cd01c44a7674474c16f9b09
Jaclyn John <84813157+jaclynamberjohn@users.noreply.github.com>
2021-07-02 10:29:49 -0700

    Update makeamovie.m

--- calibration/makeamovie.m
+++ calibration/makeamovie.m
@@ -1,17 +1,16 @@
-function video = makeamovie(filepath,wavelength,nSteps)
+function video = makeamovie(filepath,wavelength)
 %filpath - to h5 file where measurement data is held 
 %wavelength 
 %nSteps for stepper should be 64 
 
 filepath = '/Users/jaclynjohn/Documents/MATLAB/aircal.h5'; %change 
 wavelength = 460;
-nSteps = 64;
 
 video = VideoWriter('D:\Measurements\Waveguides\option1\530') %path where avi file will be saved
 video.FrameRate = .1 
 open(video)
 
-for ii = 1:nSteps 
+for ii = 1:64
     group_name   = strcat('/images/wave',num2str(wavelength),'/meas',num2str(ii));
     images = h5read(filepath,strcat(group_name,'/imagedata'));
     %pause(0.5); %slow down frames
